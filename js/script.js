1b8ddf2b755eadb7f7e25b99d31c73cfea98a265
AndreAlmeida46 <andre.r.almeida@ua.pt>
2024-12-02 10:56:31 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //ficheiro de script
+
+(function() {
+    window.addEventListener('load', function() {
+        var form = document.getElementById('contactForm');
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }, false);
+})();
+
+function resetForm() {
+    var form = document.getElementById('contactForm');
+    form.reset();
+    form.classList.remove('was-validated');
+}
